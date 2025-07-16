a9198d93d4e959d94b00c302a126c4775700cefd
JAYARANIMANIMRAN <jayarani.m2023aiml@sece.ac.in>
2025-07-16 23:17:22 +0530

    Cors Configuration Implementation

--- src/main/java/com/example/SpringBoot/config/SpringConfigration.java
+++ src/main/java/com/example/SpringBoot/config/SpringConfigration.java
@@ -1,62 +1,84 @@
 package com.example.SpringBoot.config;
+
+import com.example.SpringBoot.jwt.JwtAuthenthicationfilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.List;
 
 @Configuration
 @EnableMethodSecurity
 public class SpringConfigration {
 
+    @Autowired
+    private JwtAuthenthicationfilter jwtAuthenticationFilter;
+
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+
     }
 
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf((csrf)->csrf.disable())
-                .authorizeHttpRequests(auth-> {
-//              auth.requestMatchers(HttpMethod.POST,"/employee").hasRole("ADMIN");
-//              auth.requestMatchers(HttpMethod.PUT,"/employee").hasRole("ADMIN");
-//              auth.requestMatchers(HttpMethod.DELETE,"/employee").hasRole("ADMIN");
-//              auth.requestMatchers(HttpMethod.GET,"/**").hasAnyRole("ADMIN","USER");
-                    auth.anyRequest().authenticated();
-                })
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
 
-                .httpBasic(Customizer.withDefaults());
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:5173",
+                "https://ems-1wff.onrender.com"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/", config);
+        return source;
+
+    }
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(cors -> cors.configurationSource(request -> {
+                    CorsConfiguration corsConfig = new CorsConfiguration();
+                    corsConfig.setAllowedOrigins(List.of("http://localhost:5173")); // Frontend origin
+                    corsConfig.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                    corsConfig.setAllowedHeaders(List.of("*"));
+                    corsConfig.setAllowCredentials(true);
+                    return corsConfig;
+
+                }))
+
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/**").permitAll()
+
+                        .requestMatchers(HttpMethod.GET, "/**").hasAnyRole("USER", "ADMIN")
+                        .anyRequest().authenticated()
+
+                )
+
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
 
-
     }
-
-
-
-
-
-//  @Bean
-//  UserDetailsService userDetailsService()
-//    UserDetails admin = User.builder()
-//            .username("admin")
-//            .password(passwordEncoder().encode("admin"))
-//            .roles("ADMIN")
-//            .build();
-//    UserDetails ksp = User.builder()
-//            .username("ksp")
-//            .password(passwordEncoder().encode("ksp@1009"))
-//            .roles("USER")
-//            .build();
-//    return new InMemoryUserDetailsManager(admin,ksp);
-//  }
-
 
 }

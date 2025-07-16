f8e5e5ff792ae23cff44a9497208fc8c29f93b40
JAYARANIMANIMRAN <jayarani.m2023aiml@sece.ac.in>
2025-07-16 10:25:42 +0530

    EmployeeServiceTest

---
+++ src/main/java/com/example/SpringBoot/controller/Hello.java
@@ -0,0 +1,9 @@
+package com.example.SpringBoot.controller;
+
+public class Hello {
+
+
+    public String HelloTest(){
+        return "Hello test";
+    }
+}
---
+++ src/test/java/com/example/SpringBoot/controllers/HelloTest.java
@@ -0,0 +1,16 @@
+package com.example.SpringBoot.controllers;
+
+import com.example.SpringBoot.controller.Hello;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HelloTest {
+    @Test
+    public void testHelloTest(){
+        Hello hell = new Hello();
+        String str = hell.HelloTest();
+        System.out.println(str);
+        assertEquals("Hello test",str);
+    }
+}
